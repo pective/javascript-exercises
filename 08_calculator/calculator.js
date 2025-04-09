0085688d5c0fcdad53b09febc8d5daf4e7f278d7
pective <pective@proton.me>
2025-04-09 19:20:15 +0200

    ex 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+	let sum = 0;
+  for(const arg of args) {
+    sum += arg;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  let mul = 1;
+  for(const arg of args) {
+    mul = mul * arg;
+  }
+  return mul;
 };
 
-const power = function() {
-	
+const power = function(n1, n2) {
+	return Math.pow(n1, n2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let fac = 1;
+  for (; num > 0; num--) {
+    fac = fac*num;
+  }
+  return fac;
 };
 
 // Do not edit below this line
