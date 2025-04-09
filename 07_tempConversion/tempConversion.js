ee915b36627cb43ad8a6bb66e6e4c4ebc98eacfb
pective <pective@proton.me>
2025-04-09 18:32:42 +0200

    ex 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function( temp ) {
+  let convTemp = Math.round(((temp - 32) * 5/9) * 10) / 10;
+  return convTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function( temp ) {
+  let convTemp = Math.round((temp * 9/5 + 32) * 10) / 10;
+  return convTemp;
 };
 
 // Do not edit below this line
