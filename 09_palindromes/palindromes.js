8daeb3461efd1ab86726726b382aeb38ca822c75
pective <pective@proton.me>
2025-04-10 10:59:47 +0200

    ex 9 kinda janky but works

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
 
+const palindromes = function ( str ) {
+    let cleanStr = str.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
+    cleanStr = cleanStr.replace(/\s+/g, '');
+    let lowStr = cleanStr.toLowerCase();
+    
+    let revStr = lowStr.split('').reverse().join('')
+
+    // console.log(str);
+    // console.log(revStr);
+
+    if ( revStr === lowStr) {
+        return true;
+    } else return false;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
