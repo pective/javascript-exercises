1403f807aa3622048f2686a542174cae426ebb25
pective <pective@proton.me>
2025-05-11 18:26:19 +0200

    Got the 1st 2 tests done in ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+function getAge(death, birth) {
+    if (!death) {
+        let death = new Date().getFullYear()
+    }
+    return death - birth;
+}
 
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, current) => {
+        const currentAge = getAge(current.yearOfDeath, current.yearOfBirth);
+        const oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth)
+
+        return oldestAge > currentAge ? oldest : current;
+    })
 };
 
 // Do not edit below this line
