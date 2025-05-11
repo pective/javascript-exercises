fbe5e3bb9f02e255a14ac2830fb805648ba841a9
pective <pective@proton.me>
2025-05-11 16:18:03 +0200

    ex 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let titles = [];
+    arr.forEach((item, index) => {
+        titles.push(item.title);
+    });
+    return titles;
 };
 
 // Do not edit below this line
